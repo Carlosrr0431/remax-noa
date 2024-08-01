b1d033e48090445b39f9f3ae517154c6f8a900fa
Carlos RR <carlos.facundo.rr@gmail.com>
2024-08-01 14:50:29 -0300

    Mensaje

--- src/app/(components)/MapGmail.jsx
+++ src/app/(components)/MapGmail.jsx
@@ -1,10 +1,9 @@
 "use client";
 import React from 'react';
-import { YouTubeEmbed } from '@next/third-parties/google'
-import { GoogleMapsEmbed } from '@next/third-parties/google'
+
 
 const Mapa = () => {
- 
+
 
   // style={{ height: "50vh", width: "50vw" }}
 
@@ -57,12 +56,12 @@ const Mapa = () => {
     // <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3621.916266010462!2d-65.40195892588464!3d-24.7983206081962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x941bc39e1ae3949b%3A0xee7382ed15e6a106!2sJ.%20A.%20Fern%C3%A1ndez%20630%2C%20Salta!5e0!3m2!1ses-419!2sar!4v1708797876215!5m2!1ses-419!2sar" width="600" height="450" className="border-0 rounded-[25px]" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade" frameborder="0" ></iframe>
 
     <div className=''>
-      {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3621.916266010462!2d-65.40195892588464!3d-24.7983206081962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x941bc39e1ae3949b%3A0xee7382ed15e6a106!2sJ.%20A.%20Fern%C3%A1ndez%20630%2C%20Salta!5e0!3m2!1ses-419!2sar!4v1708797876215!5m2!1ses-419!2sar" width="600" height="450" className="border-0 h-[50vh] w-[50vw] rounded-[15px] shadow-2xl shadow-black/60 "    frameborder="0" aria-hidden="true" tabindex="-1" ></iframe> */}
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3621.916266010462!2d-65.40195892588464!3d-24.7983206081962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x941bc39e1ae3949b%3A0xee7382ed15e6a106!2sJ.%20A.%20Fern%C3%A1ndez%20630%2C%20Salta!5e0!3m2!1ses-419!2sar!4v1708797876215!5m2!1ses-419!2sar" width="600" height="450" className="border-0 h-[50vh] w-[50vw] rounded-[15px] shadow-2xl shadow-black/60 " frameborder="0" aria-hidden="true" tabindex="-1" ></iframe>
 
-      <YouTubeEmbed videoid="b8U5cBu0tMM"   height={400} width={600} />
-    
 
-   
+
+
+
     </div>
   )
 };
--- src/app/(pages)/ubicacion/page.jsx
+++ src/app/(pages)/ubicacion/page.jsx
@@ -10,7 +10,7 @@ const Ubicacion = () => {
       <div
       >
         <h2 className='montserrat text-[35px] leading-tight md:text-[40px] font-normal md:leading-[1.3] mb-4'>¿COMO LLEGAR?</h2>
-        <p className='montserrat text-start text-wrap w-[400px] text-[30px] leading-tight md:text-[25px]  text-white md:leading-[1.3] mb-4'><span className="texto-borde"> Haz clic</span> en "cómo llegar" para ver la ruta según tu medio de transporte.</p>
+        <p className='montserrat text-start text-wrap w-[400px] text-[30px] leading-tight md:text-[25px]  text-white md:leading-[1.3] mb-4'><span className="texto-borde"> Haz clic</span> en cómo llegar para ver la ruta según tu medio de transporte.</p>
       </div>
 
       <div
